5c7068230c43f1d19284a93a96cac0733cbd6281
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-01-30 20:32:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Logo from './assets/img/covid-19.svg'
 import './App.css';
 
 const App=()=>{
@@ -9,7 +10,9 @@ const App=()=>{
      <React.Fragment>
         <CssBaseline />
         <Container maxWidth="lg">
-         
+        <Grid container>
+           <img  src={Logo} alt='logo'/>
+        </Grid>
         </Container>
      </React.Fragment>
   );
